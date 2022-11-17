3a88f27117d358934b95465968c96655fda7c90f
Sang Oh <baul3oh@gmail.com>
2022-11-17 05:12:41 -0600

    save before working again

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static("public"));
 
 app.get("/notes", (req, res) => {
